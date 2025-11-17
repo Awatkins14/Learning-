f70b841cc4c7f7094b8f413682467dcc133a8ce8
awatkins14 <awatkins1@ycp.edu>
2025-11-17 11:57:56 -0500

    pythonProject1 done
    learn how to do string

---
+++ python/pythonProject/main.py
@@ -0,0 +1,17 @@
+# learn ing how to do string and print them out!!!!!!!
+
+print("hello World!!!!!!!!!!!!!!!\n")
+
+print(' i am iron man \n')
+
+# this is my test
+
+print(""" Hello World
+    i am iron man 
+    yes that is me \n""")
+
+print("i am iron man \n  " + " yes i am \n " + " that is me \n")
+
+print("i am a cat " * 10 )
+
+
