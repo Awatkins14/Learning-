2a45374d7603ac518869010ad49b7996de5fafd1
awatkins14 <awatkins1@ycp.edu>
2025-11-17 19:47:45 -0500

    pythonPart1

---
+++ python/pythonProject1/main.py
@@ -0,0 +1,14 @@
+# lets build robot barista!!
+
+
+print("Hello, welcome to The Crew\n ")
+
+name = input("What is your Name? \n")
+
+print("Hello... " + name + ", thank you for coming in today \n" )
+
+print("today on the menu we have a great food opition \n")
+order = input(" beef\n cow \n tech cow\n cow cow \n mow mow \n")
+
+print ("just cheking you order the " + order)
+
